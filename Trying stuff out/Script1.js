c195da82a603972c8ad7bf08140ab3d69e01fb0a
nave17 <yousifbinyehia@gmail.com>
2022-11-30 12:24:32 +0200

    Current 11/30/2022

---
+++ Trying stuff out/Script1.js
@@ -0,0 +1,5 @@
+// JavaScript source code
+
+function HospitalCash(capacity, patients, moneyperpatient){
+if(patients>=capacity){return capacity*moneyperpatient;} else {return patients*moneyperpatient;}
+}
